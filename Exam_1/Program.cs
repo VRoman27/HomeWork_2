6656683c7259d0e07ecc536b38d10b3295c4760d
VRoman27 <romavolodin1@rambler.ru>
2022-12-02 22:46:40 +0300

    Add Exam_1

---
+++ Exam_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+bool isParsed = false;
+int num = 0;
+
+while (!isParsed | !(num < 1000 & num > 99))
+{
+    Console.Write("Введите число: ");
+    isParsed = int.TryParse(Console.ReadLine(), out num);
+}
+
+int midDigit = (num % 100)/10;
+
+Console.WriteLine(midDigit);
