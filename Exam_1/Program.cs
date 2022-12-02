717fb4a35a9478881b9ed298bd0032809bb86d68
VRoman27 <romavolodin1@rambler.ru>
2022-12-02 23:48:31 +0300

    Fix Exam_1

--- Exam_1/Program.cs
+++ Exam_1/Program.cs
@@ -4,12 +4,21 @@
 bool isParsed = false;
 int num = 0;
 
-while (!isParsed | !(num < 1000 & num > 99))
+while (!isParsed)
 {
     Console.Write("Введите число: ");
     isParsed = int.TryParse(Console.ReadLine(), out num);
+    if (isParsed & !(num < 1000 & num > 99))
+    {
+        Console.WriteLine("Не трёхзначное число!");
+        isParsed = false;
+    }
+    else if (!isParsed)
+    {
+        Console.WriteLine("Это не число!");
+    }
 }
 
-int midDigit = (num % 100)/10;
+int midDigit = (num % 100) / 10;
 
 Console.WriteLine(midDigit);
